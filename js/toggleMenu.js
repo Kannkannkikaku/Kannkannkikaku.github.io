2b8678a04551ebd7b937d7336c6975bd3a33c770
Kannkannkikaku <Kannkannkikaku@gmail.com>
2025-06-15 15:59:06 +0900

    サイドメニュー外クリックでも閉じるように

--- js/toggleMenu.js
+++ js/toggleMenu.js
@@ -1,7 +1,25 @@
 function toggleMenu() {
-    document.getElementById('menu').classList.toggle('open');
+  document.getElementById('menu').classList.toggle('open');
+}
+
+function closeMenu() {
+  document.getElementById('menu').classList.remove('open');
 }
 
 window.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('menu').classList.remove('open');
+  const menu = document.getElementById('menu');
+  menu.classList.remove('open');
+
+  // サイドメニュー自体をクリックで閉じる
+  menu.addEventListener('click', function () {
+    closeMenu();
+  });
+
+  // メニュー内のリンクをクリックしたときに閉じないようにする
+  const links = menu.querySelectorAll('a');
+  links.forEach(link => {
+    link.addEventListener('click', function (event) {
+      event.stopPropagation(); // 閉じるイベントが発動しないようにする
+    });
+  });
 });
